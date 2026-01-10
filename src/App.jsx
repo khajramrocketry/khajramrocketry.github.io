aab3342b019ab094c6f45f78bf56ba3b8bd8762a
Atharva Geete <angeete@umich.edu>
2026-01-10 17:54:09 -0500

    added react compatibility

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>Khajra Model Rocketry</header>
+      <main>
+        {
+          <div>
+          <div class="website_header">
+            <img src="Khajra_logo.jpg" alt="logo"></img>
+            <h1>Khajra Model Rocketry at U of M</h1>
+          </div>
+            
+          <p>We are an engineering project team specializing in launching rockets and 
+            having our members learn the process of building and launching their own rockets</p>
+          </div>
+        }
+      </main>
+
+    </>
+  )
+}
+
+export default App
