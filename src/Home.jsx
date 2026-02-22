390d7d1bff8c14ee452d8463ce18d3320bdd9391
Atharva Geete <angeete@umich.edu>
2026-02-22 11:11:15 -0500

    Styling changes without logo

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { MdRocketLaunch } from "react-icons/md";
+import { GrInstagram, GrLinkedin } from "react-icons/gr";
 import './App.css';
 
 function Home() {
@@ -9,38 +10,36 @@ function Home() {
         <div>
           <div className="website_header">
             <img src="/Khajra_logo.jpg" alt="logo" className='logo_image'/>
-            <h1 className="title">Khajra Model Rocketry at U of M</h1>
-            <div className="rocket_icon_title"><MdRocketLaunch /></div>
+            
+            <ul className="sidebar_links">
+                <li>
+                  <Link to="/">Home</Link>
+                  <div className="rocket_icon_link" style={{color:"rgb(255, 255, 255)"}}></div>
+                </li>
+                <li>
+                  <Link to="/rocket_data">Rocket Data</Link>
+                  <div className="rocket_icon_link" style={{color:"rgb(255, 255, 255)"}}></div>
+                </li>
+                <li>
+                  <Link to="/resources">Resources</Link>
+                  <div className="rocket_icon_link" style={{color:"rgb(255, 255, 255)"}}></div>
+                </li>
+            </ul>
+            <div className="social_logos" style={{color:"rgb(255, 255, 255)"}}>
+              <GrInstagram />
+              <GrLinkedin />
+            </div>
+
           </div>
+
+
+          <div className="background_image_wrapper">
+            <img src="/L1_rocket_image.webp" alt="background" className='background_image'/>
+            <div className="blue_tint_overlay"></div>
+          </div>
+
           
           <div className="content_box_homepage">
-            <div className="sidebar_container">
-              <svg xmlns="http://www.w3.org/2000/svg" 
-                   className="sidebar_polygon">
-                <rect x="0" y="0" width="100%" height="100%" fill="rgb(20, 8, 80)"/>
-              </svg>
-              
-              <ul className="sidebar_links">
-                <li>
-                  <div className="link">
-                  <Link to="/">Home</Link>
-                  <div className="rocket_icon_link" style={{color:"rgb(255, 243, 18)"}}><MdRocketLaunch /></div>
-                  </div>
-                </li>
-                <li>
-                  <div className="link">
-                  <Link to="/rocket_data">Rocket Data</Link>
-                  <div className="rocket_icon_link" style={{color:"rgb(255, 243, 18)"}}><MdRocketLaunch /></div>
-                  </div>
-                </li>
-                <li>
-                  <div className="link">
-                  <Link to="/resources">Resources</Link>
-                  <div className="rocket_icon_link" style={{color:"rgb(255, 243, 18)"}}><MdRocketLaunch /></div>
-                  </div>
-                </li>
-              </ul>
-            </div>
 
             <div className="main_content">
               <div className="internal_header">
